9291f397337325cbeafaa481d041415e41bc9638
Kodefucius <NightKhaos>
2012-11-17 13:43:47 +0200

    Added mirc client/server

---
+++ Mirc Lab1/Mirc/MircClient/MircClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ServiceModel.Security;
+using System.Threading;
+using MircClient.MircService;
+
+namespace MircClient
+{
+    internal class Program
+    {
+        private static MircServiceClient _mircServiceClient;
+
+        private static void Main(string[] args)
+        {
+            _mircServiceClient = new MircServiceClient();
+            string username = "User" + Guid.NewGuid().ToString();
+            new Thread(ReadMessages).Start();
+
+            while (true)
+            {
+                Console.Write("Gigi : ");
+                string readLine = Console.ReadLine();
+                _mircServiceClient.SendMessage(new Message{Info = readLine,Username = username});
+            }
+        }
+
+        public static void ReadMessages()
+        {
+            while(true)
+            {
+                Console.Clear();
+                string[] messages = _mircServiceClient.GetMessages();
+                foreach (var message in messages)
+                {
+                    Console.WriteLine(message);
+                }
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
